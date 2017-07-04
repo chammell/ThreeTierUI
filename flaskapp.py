66eddaedade2675e3f4ea72daad7d4a55de9cd79
chammell <chammell@charleshammell.com>
2017-07-04 13:06:47 -0400

    Adding render_template

--- flaskapp.py
+++ flaskapp.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
 	return 'Hello from Flaskapp'
 
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+	return render_template('example.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
